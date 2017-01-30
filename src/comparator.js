dc3c4be11f9f53a1296d1e8740f4f626ade9fb03
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-01-30 23:24:57 +0100

    refactor(comparator): comply with eslint

--- src/comparator.js
+++ src/comparator.js
@@ -1,21 +1,21 @@
 export default class Comparator {
   constructor(jsDiff) {
-    this._cache = new Map();
-    this._jsDiff = jsDiff;
+    this.cache = new Map();
+    this.jsDiff = jsDiff;
   }
 
   getDiff(key, newValue) {
-    let previousValue = this._cache.get(key) || '';
-    return this._jsDiff.diffTrimmedLines(previousValue, newValue);
+    let previousValue = this.cache.get(key) || '';
+    return this.jsDiff.diffTrimmedLines(previousValue, newValue);
   }
 
   diffAndUpdate(key, newValue) {
     let diff = this.getDiff(key, newValue);
-    this._cache.set(key, newValue);
+    this.cache.set(key, newValue);
     return diff;
   }
 
   getCache() {
-    return this._cache;
+    return this.cache;
   }
 }
