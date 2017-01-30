0b3e469c940e8ac1293dc246cbbd00c9619d6a76
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-01-30 22:59:21 +0100

    refactor(comparator): use Map for caching, comply to test interface

--- src/comparator.js
+++ src/comparator.js
@@ -1,11 +1,21 @@
 export default class Comparator {
-  constructor(jsDiff, cache) {
+  constructor(jsDiff) {
+    this._cache = new Map();
     this._jsDiff = jsDiff;
-    this._cache = cache;
   }
 
   getDiff(key, newValue) {
     let previousValue = this._cache.get(key) || '';
     return this._jsDiff.diffTrimmedLines(previousValue, newValue);
+  }
+
+  diffAndUpdate(key, newValue) {
+    let diff = this.getDiff(key, newValue);
+    this._cache.set(key, newValue);
+    return diff;
+  }
+
+  getCache() {
+    return this._cache;
   }
 }
